8c43e40b81e5589fb3d2ae139d88b0baea34b1b9
JackTheRipper2187 <ulilf442@gmail.com>
2023-11-27 12:11:47 +0700

    modifikasi percobaan 5 menampilkan rata-rata setiap kota

--- NestedLoop22.java
+++ NestedLoop22.java
@@ -8,17 +8,14 @@ public class NestedLoop22 {
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("City: " + i);
+            double sum = 0;
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Day " + (j + 1) + ": ");
                 temps[i][j] = scanner.nextDouble();
+                sum += temps[i][j];
             }
-            System.out.println();
-        }
-
-        for (double[] cityTemps : temps) {
-            for (double temp : cityTemps) {
-                System.out.print(temp + " ");
-            }
+            double average = sum / temps[i].length;
+            System.out.println("Average temperature for City " + i + ": " + average);
             System.out.println();
         }
     }
